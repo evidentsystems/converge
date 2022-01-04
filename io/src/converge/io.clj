ee2b43062a8d50ae6f7f5eec4805ae9fc28896e5
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-01-03 19:40:05 -0700

    `io` module uses Nippy instead of transit-msgpack for serialization

--- io/src/converge/io.clj
+++ io/src/converge/io.clj
@@ -1,34 +1,37 @@
 (ns converge.io
   (:refer-clojure :exclude [read])
   (:require [clojure.java.io :as io]
-            [cognitect.transit :as t]
+            [clojure.string :as string]
+            [taoensso.nippy :as nippy]
+            converge.nippy
             [converge.api :as convergent]
-            [converge.domain :as domain]
-            [converge.transit :as transit])
-  (:import [java.security MessageDigest]
-           [java.io ByteArrayOutputStream]))
+            [converge.domain :as domain])
+  (:import [java.security MessageDigest]))
 
+;; TODO: validate filename hash against content?
 (defn read-file
   [^java.io.File file]
   (when (.isFile file)
     (try
-      (with-open [in (io/input-stream file)]
-        (t/read (t/reader in :msgpack {:handlers transit/read-handlers})))
+      (nippy/thaw-from-file file)
       (catch Exception e
         (binding [*out* *err*]
           (println (.getLocalizedMessage e)))
         nil))))
 
 (defn ^java.io.File root-file
-  [dirname]
-  (io/file dirname "root.converge"))
+  [^java.io.File dir]
+  (some (fn [^java.io.File f]
+          (and (string/ends-with? (.getName f) ".root.converge")
+               f))
+        (file-seq dir)))
 
 (defn from-directory
   [dirname]
   (let [dir (io/file dirname)
         {id       :source
          root-ops :ops}
-        (some-> dirname
+        (some-> dir
                 root-file
                 read-file)]
     (when (and (.isDirectory dir)
@@ -45,25 +48,6 @@
                    agg))
                root-ops
                (file-seq dir))))))
-
-(defn pr-directory
-  ([dirname]
-   (pr-directory dirname :edn))
-  ([dirname value-format]
-   (when-let [cref (from-directory dirname)]
-     (let [value @cref]
-       (case value-format
-         :edn
-         (pr value)
-
-         :transit-json
-         (t/write (t/writer System/out :json) value)
-
-         :transit-json-verbose
-         (t/write (t/writer System/out :json-verbose) value)
-
-         :transit-msgpack
-         (t/write (t/writer System/out :msgpack) value))))))
 
 (def file-hash-algo
   "SHA3-256")
@@ -83,26 +67,21 @@
 (defn sync-directory
   [source-ref dirname]
   (assert (convergent/convergent? source-ref) "First argument to sync-directory must be a ConvergentRef")
-  (if-let [dest-ref (from-directory dirname)]
-    (let [patch (convergent/patch-from-clock source-ref
-                                             (convergent/clock dest-ref))]
-      (when patch
-        (let [baos (ByteArrayOutputStream.)
-              _    (t/write (t/writer baos :msgpack {:handlers transit/write-handlers})
-                            patch)
-              file (io/file dirname (str (file-hash (.toByteArray baos))
-                                         ".converge"))]
-          (with-open [output-stream (io/output-stream file)]
-            (.writeTo baos output-stream))
-          (.getName file))))
-    (let [file (root-file dirname)]
-      (.mkdirs (io/file dirname))
-      (with-open [output-stream (io/output-stream file)]
-        (t/write (t/writer output-stream :msgpack {:handlers transit/write-handlers})
-                 (domain/->Patch
-                  (convergent/ref-id source-ref)
-                  (convergent/ref-log source-ref))))
-      (.getName file))))
+  (let [[patch root?]
+        (if-let [dest-ref (from-directory dirname)]
+          [(convergent/patch-from-clock
+            source-ref
+            (convergent/clock dest-ref))]
+          [(domain/->Patch
+            (convergent/ref-id source-ref)
+            (into {} (convergent/ref-log source-ref)))
+           true])
+        ba   (nippy/freeze patch)
+        file (io/file dirname (str (file-hash ba) (when root? ".root") ".converge"))]
+    (.mkdirs (io/file dirname))
+    (with-open [output-stream (io/output-stream file)]
+      (.write output-stream ba))
+    (.getName file)))
 
 (comment
 
@@ -112,8 +91,6 @@
   (sync-directory r dirname)
 
   (def r (from-directory dirname))
-
-  (pr-directory dirname)
 
   @r
 
