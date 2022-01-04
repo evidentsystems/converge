c5404f5f39fa355cb91e55896695a974962d5441
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-01-04 00:52:03 -0700

    Fix converge CLJS ci w/ puppeteer

--- converge/karma.conf.js
+++ converge/karma.conf.js
@@ -11,6 +11,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
+
 module.exports = function (config) {
     config.set({
         browsers: ['ChromeHeadless'],
