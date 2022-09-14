6c118e1121822b1162a563fb86aa5f0847fcc3c4
Bobby Calderwood <8336+bobby@users.noreply.github.com>
2022-09-14 10:33:35 -0600

    Simpler build setup; wrapper for filesystem/sync-directory

--- java/src/main/java/converge/java/Converge.java
+++ java/src/main/java/converge/java/Converge.java
@@ -1,9 +1,21 @@
 package converge.java;
 
 import converge.domain.ConvergentRef;
+import clojure.lang.IFn;
+import clojure.java.api.Clojure;
+
+import java.io.File;
 
 public class Converge {
-    public static void sync(ConvergentRef ref) {
+    // private static final IFn reset = Clojure.var("clojure.core", "reset!");
+    // private static final IFn deref = Clojure.var("clojure.core", "deref");
+    private static final IFn syncDirectoryClojure = Clojure.var("converge.storage.filesystem", "sync-directory");
 
+    public static String syncDirectory(ConvergentRef ref, File directory) {
+        if (directory.isDirectory()) {
+            return (String) syncDirectoryClojure.invoke(ref, directory.getName());
+        } else {
+            throw new IllegalArgumentException("Syncing a convergent ref requires a directory");
+        }
     }
 }
